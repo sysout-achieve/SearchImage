7c8b27954bd89a96981fdbd3f8996d58c4db9aae
gunt <0408gun@gmail.com>
2021-02-21 21:36:01 +0900

    [feat] DI(Hilt) 기본 모듈 구성

--- app/src/main/java/com/gunt/searchimage/ui/MainActivity.kt
+++ app/src/main/java/com/gunt/searchimage/ui/MainActivity.kt
@@ -2,11 +2,16 @@ package com.gunt.searchimage.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.gunt.searchimage.R
+import com.gunt.searchimage.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
