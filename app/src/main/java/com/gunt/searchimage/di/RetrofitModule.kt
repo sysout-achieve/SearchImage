336d22869b5997b48521502a99b8dcfa21eb26cf
gunt <0408gun@gmail.com>
2021-02-21 21:28:01 +0900

    [feat] DI(Hilt) 기본 모듈 구성

---
+++ app/src/main/java/com/gunt/searchimage/di/RetrofitModule.kt
@@ -0,0 +1,30 @@
+package com.gunt.searchimage.di
+
+import com.gunt.searchimage.BuildConfig
+import com.gunt.searchimage.data.repository.network.ImageDocumentService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+private const val BASE_URL = "https://dapi.kakao.com"
+
+const val KAKAO_API_KEY = BuildConfig.KAKAO_KEY
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofitApiService(): ImageDocumentService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ImageDocumentService::class.java)
+    }
+}
