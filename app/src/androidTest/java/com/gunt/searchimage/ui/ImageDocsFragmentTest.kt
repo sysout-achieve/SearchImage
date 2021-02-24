40fa7274540f1eefe5fb184a9434ce937a91bb31
gunt <0408gun@gmail.com>
2021-02-24 14:45:43 +0900

    [test] 요구사항에 맞는 텍스트 test
    전체 이미지 화면에 ImageDocument의 요구사항 데이터 출력 확인용 test

--- app/src/androidTest/java/com/gunt/searchimage/ui/ImageDocsFragmentTest.kt
+++ app/src/androidTest/java/com/gunt/searchimage/ui/ImageDocsFragmentTest.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.swipeUp
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.gunt.searchimage.R
 import com.gunt.searchimage.data.domain.ImageDocument
@@ -30,11 +31,6 @@ class ImageDocsFragmentTest {
         datetime = "2020-08-08T14:18:05.000+09:00",
         image_url = "https://t1.daumcdn.net/cfile/tistory/2420DE3B52E04A4A1A"
     )
-    private val imageDocument = ImageDocument(
-        display_sitename = "네이버블로그",
-        datetime = "2020-08-08T14:18:05.000+09:00",
-        image_url = "https://search3.kakaocdn.net/argon/130x130_85_c/JC8LrBAhH4Q"
-    )
 
     @Before
     fun setUp() {
@@ -46,11 +42,16 @@ class ImageDocsFragmentTest {
 
     @Test
     fun fragmentInViewTest() {
-        //       response 데이터에 출처 'display_sitename', 문서 작성 시간 'datetime'이 있을 경우 전체화면 이미지 밑에 표시해 줍니다.
         onView(withId(R.id.btn_back)).check(matches(isDisplayed()))
         onView(withId(R.id.txtview_sitename)).check(matches(isDisplayed()))
         onView(withId(R.id.txtview_datetime)).check(matches(isDisplayed()))
         onView(withId(R.id.view_img_detail)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun checkTextViewTest() {
+        onView(withId(R.id.txtview_sitename)).check(matches(withText(imageDocumentLongHeight.getSiteName())))
+        onView(withId(R.id.txtview_datetime)).check(matches(withText(imageDocumentLongHeight.getDateTime())))
     }
 
     @Test
