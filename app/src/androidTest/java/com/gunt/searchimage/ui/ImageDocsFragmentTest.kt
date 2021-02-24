8899d89070cbd81b4c88497bacae740095c3500a
gunt <0408gun@gmail.com>
2021-02-24 14:47:45 +0900

    [test] 요구사항에 맞는 텍스트 test
    전체 이미지 화면에 ImageDocument의 요구사항 데이터 출력 확인용 test

--- app/src/androidTest/java/com/gunt/searchimage/ui/ImageDocsFragmentTest.kt
+++ app/src/androidTest/java/com/gunt/searchimage/ui/ImageDocsFragmentTest.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.swipeUp
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.gunt.searchimage.R
 import com.gunt.searchimage.data.domain.ImageDocument
